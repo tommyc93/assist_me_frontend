5c397aab442f0f2a9e7ba583feeceb345a98caa2
Tommy Chung <tommychung@TC-MacBook-Pro.local>
2021-09-08 09:23:33 -0400

    Update

--- src/components/daily/Daily.js
+++ src/components/daily/Daily.js
@@ -1,23 +1,36 @@
 ///////////////---------Imports---------///////////////
-import React, {useState, useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import {DailyContext} from '../../context/DailyContext'
 import axios from 'axios'
 
 const Daily = (props) => {
     ///////////////---------Hook/States---------///////////////
     const [tasks, setTasks] = useContext(DailyContext)
+    const [sum, setSum] = useState(0)
 
     ///////////////---------Function---------///////////////
+    const addSum = () => {
+        let total = 0
+        for(let i = 0; i < tasks.length; i++){
+            total += tasks[i].time
+        }
+        setSum(total)
+    }
+
+    useEffect(() => {
+        addSum()
+    }, [])
 
     ///////////////---------Return---------///////////////
     return (
         <div class='card flex-even todo'>
         <h1>To Do's</h1>
+        <h4>Total Hours: {sum}</h4>
             {tasks.map((daily) => {
                 return (
                     <div class='card flex-even'>
                         <div class='card-header'>
-                            <h4>Name: {daily.name}</h4>
+                            <h4>Task: {daily.name}</h4>
                         </div>
                         <div class='card-body'>
                             <p>Hours: {daily.time}</p>
--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,21 +1,36 @@
 ///////////////---------Imports---------///////////////
-import React, {useState, useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import {TaskContext} from '../../context/TaskContext'
 import axios from 'axios'
 
 const Task = (props) => {
     ///////////////---------Hook/States---------///////////////
     const [tasks, setTasks] = useContext(TaskContext)
+    const [sum, setSum] = useState(0)
+
+    ///////////////---------Function---------///////////////
+    const addSum = () => {
+        let total = 0
+        for(let i = 0; i < tasks.length; i++){
+            total += tasks[i].time
+        }
+        setSum(total)
+    }
+
+    useEffect(() => {
+        addSum()
+    }, [])
 
     ///////////////---------Return---------///////////////
     return (
         <div class='card flex-even tasks'>
         <h1>Daily Tasks</h1>
+        <h4>Total Hours: {sum}</h4>
             {tasks.map((task) => {
                 return (
                     <div class='card flex-even'>
                         <div class='card-header'>
-                            <h4>Name: {task.name}</h4>
+                            <h4>Task: {task.name}</h4>
                         </div>
                         <div class='card-body'>
                             <p>Hours: {task.time}</p>
